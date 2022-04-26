81647a719b7b1a6d4410a5bdbf7e88fb4321aeea
valsorym <valsorym.e@gmail.com>
2022-04-26 22:32:57 +0300

    Update

--- doc.go
+++ doc.go
@@ -1,7 +1,8 @@
-// Package t13n (transliteration) ...
+// Package t13n (transliteration) implements methods for
+// converting text in unicode format to ASCII format.
 package t13n
 
-const version = "0.1.2-beta"
+const version = "0.1.3-beta"
 
 // Version returns the version of the module.
 func Version() string {
