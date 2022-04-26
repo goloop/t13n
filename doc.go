06edbb78a207ba1acf21d69334da232884e54ee3
goloop <goloop.one@gmail.com>
2022-04-26 22:25:10 +0300

    Add examples

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 // Package t13n (transliteration) ...
 package t13n
 
-const version = "0.1.0-beta"
+const version = "0.1.2-beta"
 
 // Version returns the version of the module.
 func Version() string {
