390e65c7dbaa2a1c1e8271837027524ae2220e71
goloop <goloop.one@gmail.com>
2023-06-06 19:30:30 +0300

    Update

--- doc.go
+++ doc.go
@@ -1,12 +1,3 @@
 // Package t13n (transliteration) implements methods
 // for converting Unicode text to ASCII.
 package t13n
-
-// Version of the module as {major_version}.{minor_version}.{patch_version}.
-const version = "1.2.2"
-
-// Version returns the version of the module
-// it's has a format "v{major_version}.{minor_version}.{patch_version}".
-func Version() string {
-	return "v" + version // add a "v" to the version
-}
